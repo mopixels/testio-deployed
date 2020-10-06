728bf2d90a571711df54fb37be9556eb401de085
mopixels <m.rimkus93@gmail.com>
2020-10-06 09:42:09 +0300

    Updates

---
+++ precache-manifest.4506481d8471ed70ab2746f4d8b8244a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca794cde7e8b4db8179d4f1467612a70",
+    "url": "/testio-deployed/index.html"
+  },
+  {
+    "revision": "160caf498b25e4f4b3c8",
+    "url": "/testio-deployed/static/css/main.36c4e667.chunk.css"
+  },
+  {
+    "revision": "cadee2c51915bcc718aa",
+    "url": "/testio-deployed/static/js/2.531efcd1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/testio-deployed/static/js/2.531efcd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "160caf498b25e4f4b3c8",
+    "url": "/testio-deployed/static/js/main.3b424855.chunk.js"
+  },
+  {
+    "revision": "215322e38b18ca699e64",
+    "url": "/testio-deployed/static/js/runtime-main.46e57437.js"
+  },
+  {
+    "revision": "6a348e72d401f9bb3e86a2c090ec6a63",
+    "url": "/testio-deployed/static/media/404-yoda.6a348e72.png"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/testio-deployed/static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "88823c2015ffd5fa89d567e17297a137",
+    "url": "/testio-deployed/static/media/Roboto-Light.88823c20.ttf"
+  },
+  {
+    "revision": "f6ca0908a8d40191b23ad289867844f2",
+    "url": "/testio-deployed/static/media/doors-icon.f6ca0908.svg"
+  },
+  {
+    "revision": "2e46a6691a7200c4d648b7892915ebec",
+    "url": "/testio-deployed/static/media/login-bg.2e46a669.png"
+  },
+  {
+    "revision": "d1ea3cf74aed603b689560d81cb65944",
+    "url": "/testio-deployed/static/media/logo-testio-small.d1ea3cf7.svg"
+  },
+  {
+    "revision": "d5c341d8305aae85d10ccd9bd4d582ed",
+    "url": "/testio-deployed/static/media/password-icon.d5c341d8.svg"
+  },
+  {
+    "revision": "ff88ec86d6b3c106268c0d33ea33aa71",
+    "url": "/testio-deployed/static/media/server-loading-icon.ff88ec86.svg"
+  },
+  {
+    "revision": "15aae2abd1e464f93cc127c16d439915",
+    "url": "/testio-deployed/static/media/user-icon.15aae2ab.svg"
+  }
+]);
